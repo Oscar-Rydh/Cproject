b944a27f65e3e1b1e469f86d0ed64a20f9fbd8c1
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-05-17 13:23:34 +0200

    Connecting to github

--- ourmalloc.c
+++ ourmalloc.c
@@ -11,36 +11,44 @@ void* malloc(size_t size){
     printf("head: %d\n", head);
 
     void* new_memory_address = sbrk(size);
+    printf("Allocated: %d\n", new_memory_address);
 
     if ((int*)new_memory_address == -1) {
         printf("Could not allocate memory\n");
     }
 
-    list_t entry;
-    entry.size = size;
-    entry.next = NULL;
+    list_t* entry = (list_t*) new_memory_address;
+    entry->size = size;
+    entry->next = NULL;
+    
+    //list_t entry;
+    //entry.size = size;
+    //entry.next = NULL;
     //entry.data = new_data;
 
     if (head == NULL) {
         printf("Running head");
-        head = (list_t*) new_memory_address;
-        *head = entry;
+        head = entry;
+        //*head = entry;
     } else {
         printf("Running else\n");
-        list_t current = *head;
-        list_t* next = (list_t*) new_memory_address;
-        while (current.next != NULL) {
+        list_t* current = head;
+        //list_t* next = (list_t*) new_memory_address;
+        printf("Next is %d\n", current->next);
+        printf("NULL IS %d\n", NULL);
+        while (current->next != NULL) {
             printf("Starting iteration\n");
 
-            printf("NEXT IS: %d\n", current.next);
-            list_t* current_next = current.next;
-            current = *current_next;
-            //current = *current.next;
-            printf("Cur size: %d\n", current.size);
+
+            //list_t* current_next = current.next;
+            //current = *current_next;
+            printf("Current next is: %d\n", current->next);
+            current = current->next;
+            printf("Nexit size: %d\n", current->size);
         }
-        current.next = next;
-        printf("Assigned next is: %d\n", current.next);
-        *next = current;
+        current->next = entry;
+        printf("Assigned next is: %d\n", current->next);
+        //*next = current;
     }
 
 
