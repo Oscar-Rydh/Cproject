17c74e2058c0950a5344b58d8df6e6a855c50692
Joel Klint <joel.klint@gmail.com>
2017-05-17 16:36:55 +0200

    Tracked wierd bug

--- ourmalloc.c
+++ ourmalloc.c
@@ -3,12 +3,24 @@
 #include "ourmalloc.h"
 #define N (8 * 1024*1024 / sizeof(size_t))
 
-list_t* head = NULL;
+list_t* allocated = NULL;
 
 void* malloc(size_t size){
 
-    // How much size do we need to store meta information?
-    int meta_size = sizeof(list_t) + sizeof(size);
+    /* 
+        How much size do we need to store meta information?
+
+        There seems to be some kind of bug here.
+
+        entry->data
+        is not equal to  
+        new_memory_address + meta_size
+
+        entry->data is 8 smaller. sizeof(size_t) is 8 smaller too.
+        We have subtracted sizeof(size_t) here to fix this mistake, 
+        but this is probably not a correct fix. Should this be changed?
+    */
+    int meta_size = sizeof(list_t) + sizeof(size) - sizeof(size_t);
 
     // Allocate the memory
     void* new_memory_address = (list_t*) sbrk(size + meta_size);
@@ -20,22 +32,26 @@ void* malloc(size_t size){
     list_t* entry = (list_t*) new_memory_address;
     entry->size = size;
 
-    if (head == NULL) {
-        head = entry;
+    if (allocated == NULL) {
+        allocated = entry;
     } 
     else {
-        list_t* current = head;
+        list_t* current = allocated;
         while (current->next != NULL) {
             current = current->next;
         }
         current->next = entry;
     }
 
-    return (void*) (new_memory_address + meta_size);
-}
+    // Can be used for debugging
+    // printf("%d\n", (void*) new_memory_address + meta_size);
+    // printf("%d\n", (void*) entry->data);
 
+    return (void*) entry->data;
+}
 
 int main ( int argc, char **argv ) {
     printf("Program started \n");
-    void* a = malloc(10);
+    void* a = malloc(26);
+    void* b = malloc(1);
 }
