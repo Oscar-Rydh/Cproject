c2338fdf04c698fd009eee0bf9f4c7cbf4db8bdf
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-08-22 10:25:03 +0200

    Implemented working project

--- ourmalloc.c
+++ ourmalloc.c
@@ -1,211 +1,133 @@
 #include <stddef.h>
 #include <stdio.h>
 #include "ourmalloc.h"
-#define N (8 * 1024*1024 / sizeof(size_t))
 
-list_t* head = NULL;
-list_t* freed = NULL;
+list_t available_blocks = { .next = NULL };
+
+
 
 void* malloc(size_t size){
 
-    /* 
-        How much size do we need to store meta information?
+    // Aligning size    
+    size_t final_size = (size + sizeof(list_t));
+    if (size <= 0) {
+        return NULL;
+    }
+    
+    list_t* current = &available_blocks;
+    list_t* next = available_blocks.next;
 
-        There seems to be some kind of bug here.
-
-        entry->data
-        is not equal to  
-        new_memory_address + meta_size
-
-        entry->data is 8 smaller. sizeof(size_t) is 8 smaller too.
-        We have subtracted sizeof(size_t) here to fix this mistake, 
-        but this is probably not a correct fix. Should this be changed?
-    */
-    size_t final_size = (size + sizeof(size_t)) & ~(sizeof(size_t) - 1);
-
-
-    //Check if available free memory exists
-    //If it does, remove it from the free pool and reallocate it
-    //if (freed != 0) {
-    //    list_t* current_free = freed;
-    //    list_t* previous = NULL;
-    //    while (current_free != NULL) {
-    //        if (current_free->size >= size) {
-    //            if (previous != NULL) {
-    //                //Move pointers in free memory
-    //                previous->next = current_free->next;
-    //                current_free->next = NULL;
-    //            } else {
-    //                //Move head of free
-    //                freed = current_free->next;
-    //                current_free->next = NULL;
-    //            }
-    //            if (allocated == NULL) {
-                    //Set new head for allocated memory
-    //                allocated = current_free;
-    //            } else {
-    //                // Add the new allocated memory as head.
-    //                current_free->next = allocated;
-    //                allocated = current_free;
-    //            }
-    //            return (void*) current_free->data;
-    //        }
-    //        previous = current_free;
-    //        current_free = current_free->next;
-    //    }
-    //}
-    // Allocate new memory
-    void* new_memory_address = (list_t*) sbrk(final_size);
-
-    if (new_memory_address == -1) {
-        printf("Could not allocate memory\n");
+    // Search for available blocks
+    while (next != NULL && next->size < final_size) {
+        current = next;
+        next = next->next;
     }
 
-    list_t* entry = (list_t*) new_memory_address;
-    entry->size = size;
+    // No available block was found
+    if (next == NULL) {
+        void* block;
+        block = sbrk(final_size);
 
-    if (head == NULL) {
-        head = entry;
+        if (block == -1) {
+            return NULL;
+        }
+        next = (list_t*) block;
+        next->size= final_size;
+    // Use available block
+    } else {
+        current->next = next->next;
     }
-    //else {
-    //    list_t* current = allocated;
-    //    while (current->next != NULL) {
-    //        current = current->next;
-    //    }
-    //    current->next = entry;
-    //}
-    printf("Allocating data\n");
-    return (void*) entry->data;
 
+  return  (void*) next->data;
 }
 
 void free(void* ptr) {
-    char* pointer_to_data = (char *) ptr;
-    char* pointer_to_list = ptr - sizeof(list_t);
-    printf("The pointer to the list to free is: %d\n", pointer_to_list);
-    list_t* to_be_freed = (list_t*) pointer_to_list;
-    // Find node that is pointing at our target ( call this parent )
-    //list_t* target = NULL;
 
-    //Append to free;
-    if (freed == NULL) {
-        freed = to_be_freed;
-        freed->next = NULL;
-    }else{
-        list_t* current_free = freed;
-        list_t* previous = NULL;
-        while (current_free->next != NULL) {
-            previous = current_free;
-            current_free = current_free->next;
-        }
-        current_free->next = to_be_freed;
+    if (ptr == NULL) {
+        return;
     }
+    list_t* to_be_freed = (list_t*) ((char*) ptr - sizeof(list_t));
+    list_t* current = &available_blocks;
+    list_t* next = available_blocks.next;
 
-    //Merging free blocks
-    list_t* free_iterator = head;
-    while (free_iterator->next != NULL) {
-        printf("Pointer to current is: %d \t", (char*)free_iterator);
-        printf("Pointer to next is: %d \t", free_iterator->next);
-        printf("current size is: %d\t", free_iterator->size);
-        printf("Calculated next is: %d\n", (char*) free_iterator + 4 +  free_iterator->size);
-    if ((char*) free_iterator->next == (char*) free_iterator + 4 + free_iterator->size ) {
-            free_iterator->size = free_iterator->size + sizeof(list_t) + free_iterator->next->size;
+    // Iterate to correct possition
+    while(next != NULL && to_be_freed > next) {
+        current = next;
+        next = next->next;
     }
-        free_iterator = free_iterator->next;
-    }
-    //Check if free is for head
-    //if ((int*) current != pointer_to_list) {
-    //    while ((int *) current->next != pointer_to_list) {
-    //        current = current->next;
-    //    }
-    //    target = current->next;
-    //    current->next = target->next;
-        //printf("Freeing something in the middle\n");
-    //} else {
-        //Move head of allocated
-        //printf("Moving the allocated head to %d\n", allocated->next);
-    //    allocated = allocated->next;
-    //    target = current;
-    //}
+    
 
-    // // Place target in free queue
-     //if(freed == NULL) {
-     //    freed = target;
-    //     target->next = NULL;
-     //}
-     //else {
-     //    list_t * free_current = freed;
-     //   while (free_current->next != NULL) {
-     //       free_current = free_current->next;
-     //   }
-     //    free_current->next = target;
-     //    target->next = NULL;
-    //}
-    return;
+    list_t* compare_free_with_next = to_be_freed + to_be_freed->size;
+    list_t* compare_free_with_current = (list_t*) ((char*) current + current->size);
+    // Address to be freed can be merged with current node
+    if (compare_free_with_current == to_be_freed){
+        current->size = current->size + to_be_freed->size;
+    // Address to be freed can be merged with current next
+    } else if (compare_free_with_next == next){
+        to_be_freed->size = next->size;
+        current->next = to_be_freed;
+        to_be_freed->next = next->next;
+    // Address to be freed can be merged with both nodes
+    } else if  (compare_free_with_current == to_be_freed &&
+                compare_free_with_next == next){
+        current->size = current->size + to_be_freed->size + next->size; 
+        current->next = next->next;
+    //Nothing can be merged
+    } else {
+        current->next = to_be_freed;
+        to_be_freed->next = next;
+    }
 }
 
-int main ( int argc, char **argv ) {
-    printf("Program started \n");
-    void* a = malloc(260);
-    void* b = malloc(360);
-    void* c = malloc(180);
-    void* d = malloc(500);
-    printf("Freeing a\n");
-    free(a);
-    printf("Freeing b\n");
-    free(b);
-    printf("Freeing c\n");
-    free(c);
-    printf("Freeing d\n");
-    free(d);
-    list_t* current = freed;
-    printf("The free size is: %d\n", freed->size);
-    //printf("Freehead is: %d\n", current);
-    //printf("It has size: %d\n", current->size);
-    //while (current->next != NULL) {
-    //    printf("The next pointer is: %d\n", current->next);
-    //    printf("The next size is: %d\n", current->next->size);
-    //    current = current->next;
-    //}
+void* calloc(size_t nmemb, size_t size) {
+    
+    if (nmemb == 0 || size == 0) {
+        return NULL;
+    }
 
-    //if (allocated == NULL) {
-    //    printf("All of the memory is freed\n");
-    //}
-    //void* e = malloc(260);
-    //void* f = malloc(360);
-    //void* g = malloc(180);
-    //void* h = malloc(500);
-    //void* k = malloc(1000);
-    //if (freed == NULL) {
-    //    printf("All free memory was reallocated\n");
-    //}
-    //printf("Freed is: %d\n", freed);
-    //free(k);
-    //free(h);
-    //free(f);
-    //current = freed;
-    //while(current != NULL) {
-    //    printf("Free memory sizes: %d\n", current->size);
-    //    current=current->next;
-    //}
+    size_t final_size = nmemb * size;
 
-    // SIZE WILL BE BIGGER IF A PREVIOUSLY BIGGER ALLOCATION WAS MADE
-    //void* m = malloc(800);
-    //void* l = malloc(700);
+    char* data_block = malloc(final_size);
 
-    //current = allocated;
-    //while(current != NULL) {
-    //    printf("Allocated sizes: %d\n", current->size);
-    //    current=current->next;
-    //}
+    if (data_block == NULL) {
+        return NULL;
+    }
+    size_t i = 0;
+    for (i = 0; i < final_size; i++) {
+        data_block[i] = 0;
+    }
+    return (void*) data_block;
+}
 
-    //free(e);
-    //free(l);
+void* realloc(void* ptr, size_t size){
 
-    //current = freed;
-    //while(current != NULL) {
-    //    printf("Free memory sizes: %d\n", current->size);
-    //    current=current->next;
-    //}
+    if (ptr == NULL) {
+        return malloc(size);
+    }
+    if (ptr != NULL && size == 0) {
+        free(ptr);
+        return;
+    }
+
+    void* block = malloc(size);
+    if (block == NULL) {
+        return NULL;
+    }
+    list_t* new_block = (list_t*) ((char*) block - sizeof(list_t));
+    list_t* current_block = (list_t*) ((char*) ptr - sizeof(list_t));
+    size_t new_size = 0;
+    if(current_block->size - sizeof(list_t) < size){
+        new_size = current_block->size - sizeof(list_t);
+    } else {
+        new_size = size;
+    }
+    // Copy data to reallocated block
+    size_t i;
+    for (i = 0; i < new_size; i++){
+        new_block->data[i] = current_block->data[i];
+    }
+    free(ptr);
+
+    return new_block->data;
 
 }
--- ourmalloc.h
+++ ourmalloc.h
@@ -9,5 +9,4 @@ struct list_t {
 };
 
 void* malloc(size_t size);
-
 void free(void *ptr);
