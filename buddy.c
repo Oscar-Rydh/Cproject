f01b7b5ed6b9eb5555ce807ab46c4220e456e9b8
Joel Klint <joel.klint@gmail.com>
2017-09-05 16:58:37 +0200

    documented what every method should do

--- buddy.c
+++ buddy.c
@@ -12,9 +12,35 @@ void init() {
     freelist[N] = (block_t*) sbrk((1L << N));
 }
 
-// void* malloc(size_t size) {
+// Hitta ledigt minne och returnera pekare
+void* malloc(size_t size) {
+    
+    // Hitta minsta möjliga K där 2^K >= size
 
-// }
+    // Hitta första blocket freelist[K]. 
+    // Om freelist[K] är tom, leta efter större block
+    // Om inget större block finns heller, returnera NULL
+
+    // Om det fanns ett block i freelist[K], returnera det
+    // Om det bara fanns större block, splitta ett av de rekursivt tills freelist[K] finns
+
+}
+
+// Hitta count st minnen av storlek size, fyll med nollor och returnera pekare
+void* calloc(size_t count, size_t size) {
+}
+
+// Allokera om ptr till storlek size och returnera ptr
+// Om inte minnet inte kan göras större, allokera nytt och kopiera innehåll samt free:a det gamla. Returnera pekare
+// Om ptr = NULL, utför malloc
+// Om size = 0 och ptr != NULL, allokera så lite minne som möjligt och returnera pekare
+void* realloc(void* ptr, size_t size) {
+}
+
+// Deallokera ptr
+// Om ptr = NULL, gör ingenting
+void free(void* ptr) {
+}
 
 int main ( int argc, char **argv ) {
     init();
--- buddy.h
+++ buddy.h
@@ -10,4 +10,10 @@ struct block_t {
 
 
 
-// void* malloc(size_t size);
+void* malloc(size_t size);
+
+void* calloc(size_t count, size_t size);
+
+void* realloc(void* ptr, size_t size);
+
+void free(void* ptr);
