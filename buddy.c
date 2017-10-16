3d045cfc7bf81cce92d05ead9bcafa6bf509e5e1
Joel Klint <joel.klint@gmail.com>
2017-10-16 10:32:28 +0200

    Now respects block meta size and prints freelist status for verification

--- buddy.c
+++ buddy.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <math.h>
 
-// Defines N
-#define N 10
+// Global definitions
+#define MAX_K 10
+#define BLOCK_META_SIZE sizeof(struct block_t)
+#define MAX_BLOCK_SIZE (1L << MAX_K)
 
 // The list that contains free elements
 // At place i, blocks of size s^i is stored
-block_t* freelist[N+1];
+block_t* freelist[MAX_K+1];
 
 /**
  * Internal Helpers
@@ -41,7 +43,7 @@ static void split_block(struct block_t* block) {
 }
 
 static char calc_min_kval_for_size(size_t size) {
-    int K = N;
+    int K = MAX_K;
     while(pow(2, K-1) >= size) {
         // printf("Potential size: %d\n", (int)pow(2, K-1));
         // printf("K: %d\n", K);
@@ -82,7 +84,7 @@ static struct block_t* find_free_block_with_atleast_kval(char K) {
     char temp_K = K;
     struct block_t* block;
     
-    while(!(block = get_block_with_kval_if_exist(temp_K)) && temp_K < N) {
+    while(!(block = get_block_with_kval_if_exist(temp_K)) && temp_K < MAX_K) {
         ++temp_K;
     }
 
@@ -106,10 +108,13 @@ static struct block_t* find_free_block_with_atleast_kval(char K) {
  */
 // Hitta ledigt minne och returnera pekare
 void* malloc(size_t size) {
-    printf("Size to malloc: %d\n", (int)size);
+    // Respect the BLOCK_META_SIZE
+    size_t true_size = (int)size + BLOCK_META_SIZE;
+
+    printf("Size to malloc: %d\n", (int)true_size);
 
     // Hitta minsta möjliga K där 2^K >= size
-    char K = calc_min_kval_for_size(size);    
+    char K = calc_min_kval_for_size(true_size);    
 
     // Hitta första blocket freelist[K]. 
     // Om freelist[K] är tom, leta efter större block
@@ -124,8 +129,14 @@ void* malloc(size_t size) {
         return NULL;
     }
 
-    // De gör +1, varför?
-    return block;
+    // Return block address and move pointer BLOCK_META_SIZE steps
+    printf("K of block: %d\n", (int)block->kval);
+    
+    // Print freelist status for verification
+    int temp_k;
+    for(temp_k = MAX_K; temp_k > 0; temp_k--)
+        printf("Freelist K: %d. %s\n", temp_k, freelist[temp_k] == NULL ? "false" : "true");
+    return block + BLOCK_META_SIZE;
 }
 
 // Hitta count st minnen av storlek size, fyll med nollor och returnera pekare
@@ -146,12 +157,12 @@ void free(void* ptr) {
 
 void init() {
     // Allocate all memory at first
-    block_t* starting_block = (block_t*) sbrk((1L << N));
-    starting_block->kval = N;
+    block_t* starting_block = (block_t*) sbrk((1L << MAX_K));
+    starting_block->kval = MAX_K;
     starting_block->reserved = 0;
     starting_block->prev = NULL;
     starting_block->next = NULL;
-    freelist[N] = starting_block;
+    freelist[MAX_K] = starting_block;
 }
 
 int main ( int argc, char **argv ) {
